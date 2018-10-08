a16667e8f129ef9741bb91e1752076d213dcbacf
JON <jonn.thinkrtc@gmail.com>
2018-10-08 16:03:54 -0400

    integrated userprofileinfo model and user_form as well as templates

--- user_app/admin.py
+++ user_app/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from user_app.models UserProfileInfo, User
 
 # Register your models here.
+admin.site.register(UserProfileInfo)
---
+++ user_app/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from django.contrib.auth.models import User
+from user_app.models import UserProfileInfo
+
+
+class UserForm(forms.ModelForm):
+
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta():
+        model = User
+        fields = ('username', 'email', 'password')
+
+class UserProfileInfoForm:
+
+    class Meta():
+        model = UserProfileInfo
+        fields = ('portofolio_site', 'profile_pic')
