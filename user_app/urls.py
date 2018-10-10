051203f78eb1e96b731931374220e0116bc406c4
JON <jonn.thinkrtc@gmail.com>
2018-10-09 21:25:11 -0400

    fixed login bug

--- user_app/urls.py
+++ user_app/urls.py
@@ -6,4 +6,5 @@ app_name = 'user_app'
 urlpatterns = [
     url(r'^register/$',views.register,name='register'),
     url(r'^user_login/$',views.user_login,name='login'),
+
 ]
--- user_app/views.py
+++ user_app/views.py
@@ -56,7 +56,7 @@ def user_login(request):
         user = authenticate(username=username, password=password)
 
         if user:
-            if user_active:
+            if user.is_active:
                 login(request, user)
                 return HttpResponseRedirect(reverse('index'))
             else:
